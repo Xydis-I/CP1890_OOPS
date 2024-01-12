9fc16f7b03f66636a19c339206c66aa5a1b39a5e
Xydis-I <ccb9952@hotmail.com>
2024-01-12 10:11:36 -0330

    Updated with functions.

--- Future_Value.py
+++ Future_Value.py
@@ -1,21 +1,54 @@
 import locale as lc
 
-while True:
-    monInvestment = float(input("Enter monthly investment:\t "))
-    rate = float(input("Enter yearly interest rate:\t "))
-    years = int(input("Enter number of years:\t\t "))
 
-    lc.setlocale(lc.LC_ALL, 'en-ca')
+def get_investment() -> float:
+    while True:
+        try:
+            investment = float(input("Enter monthly investment:\t "))
+            return investment
+        except ValueError:
+            print("Invalid input.")
 
-    total = 0
-    for year in range(years * 12):
-        total += monInvestment
-        total *= (1 + (rate/12/100))
 
-    print(f"\nMonthly investment: {lc.currency(monInvestment, grouping=True):>12s}")
-    print(f"Interest rate: {rate:>17.1f}")
-    print(f"Years: {years:>25d}")
-    print(f"Future Value: {lc.currency(total, grouping=True): >18s}")
+def get_rate() -> float:
+    while True:
+        try:
+            rate = float(input("Enter monthly investment:\t "))
+            return rate
+        except ValueError:
+            print("Invalid input.")
 
-    if input("\nContinue? (y/n) ").lower() != 'y':
-        break
+
+def get_year() -> int:
+    while True:
+        try:
+            year = int(input("Enter monthly investment:\t "))
+            return year
+        except ValueError:
+            print("Invalid input.")
+
+
+def main():
+    while True:
+        mon_investment = get_investment()
+        rate = get_rate()
+        years = get_year()
+
+        lc.setlocale(lc.LC_ALL, 'en-ca')
+
+        total = 0
+        for year in range(years * 12):
+            total += mon_investment
+            total *= (1 + (rate/12/100))
+
+        print(f"\nMonthly investment: {lc.currency(mon_investment, grouping=True):>12s}")
+        print(f"Interest rate: {rate:>17.1f}")
+        print(f"Years: {years:>25d}")
+        print(f"Future Value: {lc.currency(total, grouping=True): >18s}")
+
+        if input("\nContinue? (y/n) ").lower() != 'y':
+            break
+
+
+if __name__ == '__main__':
+    main()
